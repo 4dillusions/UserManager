4e70f3cd5001c33e678f43f37f97ee96bdb864f9
Robert Oze <support@4dillusions.com>
2026-04-18 17:49:30 +0200

    Update UserDTO.cs

--- Project/App4di.Dotnet.UserManager.Contracts/DTO/UserDTO.cs
+++ Project/App4di.Dotnet.UserManager.Contracts/DTO/UserDTO.cs
@@ -1,3 +1,9 @@
+/*
+4di .NET UserManager application
+Copyright (c) by 4D Illusions. All rights reserved.
+Released under the terms of the GNU General Public License version 3 or later.
+*/
+
 namespace App4di.Dotnet.UserManager.Contracts.DTO;
 
 public class UserDTO
