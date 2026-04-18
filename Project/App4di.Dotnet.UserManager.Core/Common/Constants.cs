e0b99b0bd816590d343b21b5fbe28cfcb0d04515
Robert Oze <support@4dillusions.com>
2026-04-19 01:15:18 +0200

    fix(core): align data.xml path to configuration output directory

--- Project/App4di.Dotnet.UserManager.Core/Common/Constants.cs
+++ Project/App4di.Dotnet.UserManager.Core/Common/Constants.cs
@@ -9,7 +9,7 @@ namespace App4di.Dotnet.UserManager.Core.Common;
 
 public static class Constants
 {
-    public static readonly string DataFilePath = @"..\Data\";
+    public static readonly string DataFilePath = @"Data\";
     public static readonly string XmlDataFileName = @"data.xml";
     public static readonly string JsonDataFileName = @"data.json";
 }
