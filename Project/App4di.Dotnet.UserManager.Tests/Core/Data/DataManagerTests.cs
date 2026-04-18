ecf8c37ed30873ade5e6a9aba320ca50a894930b
Robert Oze <support@4dillusions.com>
2026-04-19 00:33:16 +0200

    test(project): add modern MSTest suite and normalize line endings

---
+++ Project/App4di.Dotnet.UserManager.Tests/Core/Data/DataManagerTests.cs
@@ -0,0 +1,91 @@
+using App4di.Dotnet.UserManager.Core.Common;
+using App4di.Dotnet.UserManager.Core.Data;
+
+namespace App4di.Dotnet.UserManager.Tests.Core.Data
+{
+    [Serializable]
+    public class DataTest
+    {
+        public int Test1 { get; set; }
+        public string Test2 { get; set; } = string.Empty;
+
+        public override bool Equals(object? obj)
+        {
+            return obj is DataTest other &&
+                Test1 == other.Test1 &&
+                Test2 == other.Test2;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Test1.GetHashCode() + Test2.GetHashCode());
+        }
+    }
+
+    [TestClass]
+    public class DataManagerTests
+    {
+        static readonly string XmlPathAndFileName = Constants.DataFilePath + @"Test.xml";
+        static readonly string JsonPathAndFileName = Constants.DataFilePath + @"Test.json";
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Directory.CreateDirectory(Constants.DataFilePath);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            if (File.Exists(XmlPathAndFileName))
+                File.Delete(XmlPathAndFileName);
+
+            if (File.Exists(JsonPathAndFileName))
+                File.Delete(JsonPathAndFileName);
+        }
+
+        List<DataTest> CreateTestData()
+        {
+            List<DataTest> result = new List<DataTest>();
+
+            DataTest t1 = new DataTest { Test1 = 1, Test2 = "test1" };
+            DataTest t2 = new DataTest { Test1 = 2, Test2 = "test2" };
+            result.Add(t1);
+            result.Add(t2);
+
+            return result;
+        }
+
+        [TestMethod]
+        public void XmlTest()
+        {
+            IDataManager<DataTest> dataManager = new XmlDataManager<DataTest>();
+
+            if (File.Exists(XmlPathAndFileName))
+                File.Delete(XmlPathAndFileName);
+
+            dataManager.Save(CreateTestData(), XmlPathAndFileName);
+            Assert.IsTrue(File.Exists(XmlPathAndFileName), "XML file created");
+
+            var loadedData = dataManager.Load(XmlPathAndFileName);
+            var createdData = CreateTestData();
+            Assert.IsTrue(loadedData.Count == 2 && loadedData[0].Equals(createdData[0]) && loadedData[1].Equals(createdData[1]), "XML file load");
+        }
+
+        [TestMethod]
+        public void JsonTest()
+        {
+            IDataManager<DataTest> dataManager = new JsonDataManager<DataTest>();
+
+            if (File.Exists(JsonPathAndFileName))
+                File.Delete(JsonPathAndFileName);
+
+            dataManager.Save(CreateTestData(), JsonPathAndFileName);
+            Assert.IsTrue(File.Exists(JsonPathAndFileName), "Json file created");
+
+            var loadedData = dataManager.Load(JsonPathAndFileName);
+            var createdData = CreateTestData();
+            Assert.IsTrue(loadedData.Count == 2 && loadedData[0].Equals(createdData[0]) && loadedData[1].Equals(createdData[1]), "Json file load");
+        }
+    }
+}
---
+++ Project/App4di.Dotnet.UserManager.Tests/Core/Factory/IocTests.cs
@@ -0,0 +1,62 @@
+using App4di.Dotnet.UserManager.Core.Factory;
+
+namespace App4di.Dotnet.UserManager.Tests.Core.Factory
+{
+    public class Singleton
+    {
+        public int Test { get; set; }
+    }
+
+    public interface IFactory
+    {
+        int Test { get; set; }
+    }
+
+    public class IocTest1 : IFactory
+    {
+        int test;
+
+        public int Test
+        {
+            get { return test; }
+            set { test = value; }
+        }
+    }
+
+    public class IocTest2 : IFactory
+    {
+        int test;
+
+        public int Test
+        {
+            get { return test; }
+            set { test = value; }
+        }
+    }
+
+    [TestClass]
+    public class IocTests
+    {
+        [TestMethod]
+        public void IocSingletonTest()
+        {
+            Ioc<Singleton>.Register<Singleton>();
+            Ioc<Singleton>.Create();
+
+            Ioc<Singleton>.Instance.Test = 1;
+            Ioc<Singleton>.Instance.Test = 2;
+
+            Assert.AreEqual(2, Ioc<Singleton>.Instance.Test, "Singleton data equal");
+            Assert.IsTrue(Object.ReferenceEquals(Ioc<Singleton>.Instance, Ioc<Singleton>.Instance), "Singleton reference equal");
+        }
+
+        [TestMethod]
+        public void IocFactoryTest()
+        {
+            Ioc<IFactory>.Register<IocTest1>(() => new IocTest1() { Test = 2 });
+            Ioc<IFactory>.Create();
+
+            Assert.AreEqual(2, Ioc<IFactory>.Instance.Test, "IocTest1 data");
+        }
+    }
+}
